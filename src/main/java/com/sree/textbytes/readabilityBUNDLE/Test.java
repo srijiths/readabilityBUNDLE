6635db13df04c6ab5ca9ad1eeb3d45b62ea8f190
Sreejith.S <sreejiths@algotree.com>
2013-07-25 16:59:10 +0530

    Build added in POM and Test main class added

---
+++ src/main/java/com/sree/textbytes/readabilityBUNDLE/Test.java
@@ -0,0 +1,17 @@
+package com.sree.textbytes.readabilityBUNDLE;
+
+import com.sree.textbytes.network.HtmlFetcher;
+
+public class Test {
+	public static void main(String[] args) throws Exception {
+		Article article = new Article();
+		ContentExtractor ce = new ContentExtractor();
+		HtmlFetcher htmlFetcher = new HtmlFetcher();
+		String html = htmlFetcher.getHtml("http://blogmaverick.com/2012/11/19/what-i-really-think-about-facebook/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+Counterparties+%28Counterparties%29", 0);
+
+		article = ce.extractContent(html, "ReadabilitySnack");
+
+		System.out.println("Content : "+article.getCleanedArticleText());
+	}
+
+}
