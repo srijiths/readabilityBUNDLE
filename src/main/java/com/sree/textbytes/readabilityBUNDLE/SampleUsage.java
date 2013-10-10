99c54eb4d1a3c0620b45d5cb3a12bf16d1468770
srijiths <sreejith.pela@gmail.com>
2013-10-10 15:14:07 +0530

    Url in SampleUsgae updated

--- src/main/java/com/sree/textbytes/readabilityBUNDLE/SampleUsage.java
+++ src/main/java/com/sree/textbytes/readabilityBUNDLE/SampleUsage.java
@@ -8,7 +8,7 @@ public class SampleUsage {
 		Article article = new Article();
 		ContentExtractor ce = new ContentExtractor();
 		HtmlFetcher htmlFetcher = new HtmlFetcher();
-		String html = htmlFetcher.getHtml("http://blogmaverick.com/2012/11/19/what-i-really-think-about-facebook/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+Counterparties+%28Counterparties%29", 0);
+		String html = htmlFetcher.getHtml("http://www.firstpost.com/tech/htc-eyes-15-indian-smartphone-market-to-open-7000-outlets-1164045.html", 0);
 		
 		//System.out.println("Html : "+html);
 		article = ce.extractContent(html, "ReadabilitySnack");
